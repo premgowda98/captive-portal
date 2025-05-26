e75a7f647683f1de851fe6f3e6b1b4eb450c0069
Prem Kumar G <premkumarmvks@gmail.com>
2025-05-26 16:47:42 +0530

    dynamicall control the monitor

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"sync"
 	"time"
 
 	"github.com/premgowda98/captive-portal/captive"
 )
 
-const DetectionOn = true
+var DetectionOn = true
+var mu sync.RWMutex
 
 func main() {
 	log.Println("Starting Captive Portal Monitor...")
@@ -19,8 +23,28 @@ func main() {
 		select {
 		case <-ticker.C:
 			go func() {
+				client := &http.Client{
+					Timeout: 5 * time.Second,
+				}
+
+				resp, err := client.Get("http://localhost:8909")
+				if resp != nil {
+                    defer resp.Body.Close()
+                }
+				mu.Lock()
+				if err != nil || resp.StatusCode != http.StatusOK {
+					fmt.Println("setting detection to off")
+					DetectionOn = false
+				} else {
+					DetectionOn = true
+				}
+				mu.Unlock()
+
 				if DetectionOn {
+					fmt.Println("detection is on")
 					captive.MonitorCaptivePortal()
+				} else {
+					fmt.Println("detection is off")
 				}
 			}()
 		}
